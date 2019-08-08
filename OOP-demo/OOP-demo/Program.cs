c0c52f6b95e7b545954a0192b3a444a54998ec99
Fayazur Rahman Summit <frsummit@simplexhub.com>
2019-08-08 15:36:27 +0600

    OOP new Student create

--- OOP-demo/OOP-demo/Program.cs
+++ OOP-demo/OOP-demo/Program.cs
@@ -6,7 +6,13 @@ namespace OOP_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Student student = new Student("0123", "Salam", "01675-869321", "email", "Dhaka");
+
+            Console.WriteLine(student.Id + " " +
+                student.Name + " " +
+                student.Phone + " " +
+                student.Email + " " + 
+                student.Address);
         }
     }
 }
