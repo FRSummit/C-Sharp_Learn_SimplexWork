04554b869609ffde6ba59ece0eada6a320eb4c69
Fayazur Rahman Summit <frsummit@simplexhub.com>
2019-08-08 12:20:27 +0600

    try catch

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -3,11 +3,21 @@
 namespace HelloWorld {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Sum " + add(2, 2));
+
+            try {
+                String number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            } catch {
+                Console.WriteLine("Can't convert to byte");
+            }
+
+
+            //Console.WriteLine("Sum " + add(2, 2));
         }
 
-        public static int add(int i, int j) {
-            return (i + j);
-        }
+        //public static int add(int i, int j) {
+        //    return (i + j);
+        //}
     }
 }
