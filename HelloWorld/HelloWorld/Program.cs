066b79ee62fe78e18ec69f9c0bd04df625995448
Fayazur Rahman Summit <frsummit@simplexhub.com>
2019-08-08 12:12:02 +0600

    sum of 2 number

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -3,8 +3,11 @@
 namespace HelloWorld {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sum " + add(2, 2));
+        }
+
+        public static int add(int i, int j) {
+            return (i + j);
         }
     }
 }
